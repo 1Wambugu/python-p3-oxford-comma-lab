c8d64fdfcbfc7e27b8661a4a203803e547425b92
1Wambugu <martingakuya@gmail.com>
2023-10-03 22:23:40 +0300

    completed oxford comma lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,12 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 0:
+        return ""
+    elif len(items) == 1:
+        return str(items[0])
+    elif len(items) == 2:
+        return f"{items[0]} and {items[1]}"
+    else:
+        # Join all elements except the last one with commas
+        comma_separated = ", ".join(map(str, items[:-1]))
+        # Add "and" before the last element
+        return f"{comma_separated}, and {items[-1]}"
